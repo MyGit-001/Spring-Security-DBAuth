9093a16f9b8545e3f25924696d6aae6bdfbd657d
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-05 23:14:48 +0530

    Starting DB Auth

---
+++ src/test/java/com/example/SpringSecurityDBAuth/SpringSecurityDBAuth/SpringSecurityDbAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityDBAuth.SpringSecurityDBAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDbAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
