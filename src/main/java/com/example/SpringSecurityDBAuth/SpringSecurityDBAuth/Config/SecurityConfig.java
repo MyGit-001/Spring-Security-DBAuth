131f15313abdab64d96e110d67cb41530e2daea4
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-09 23:16:15 +0530

    Adding UserDetailService -> Hardcoding roles in the memory

--- src/main/java/com/example/SpringSecurityDBAuth/SpringSecurityDBAuth/Config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDBAuth/SpringSecurityDBAuth/Config/SecurityConfig.java
@@ -5,12 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        UserDetails Student = User.builder()
+                .username("stud")
+                .password("{noop}studPass")
+                .roles("STUD")
+                .build();
+
+        UserDetails Teacher = User.builder()
+                .username("teach")
+                .password("{noop}teachPass")
+                .roles("TEACH")
+                .build();
+
+        return new InMemoryUserDetailsManager(Student, Teacher);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
